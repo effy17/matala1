1e6c68b896110f45cea814c8067a78a49ef891f9
effy <effy@EffysPC>
2016-03-19 21:10:00 +0200

    first entry

---
+++ readingfiles/src/readingfiles/Graph.java
@@ -0,0 +1,196 @@
+package readingfiles;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+import java.io.BufferedReader;
+
+import java.io.FileReader;
+
+import java.io.IOException;
+
+import java.util.regex.Pattern;
+
+public class Graph {
+
+	private static double mat[][];
+	private static int points;
+	private static String mainfile;
+	
+	public Graph(){
+		this.mat=new double[0][0];
+		points=0;
+		mainfile="";
+	}
+	
+	public double[][] graph(){
+		return mat;
+	}
+	
+	public void setgraph(double g[][]){
+		mat=g;
+	}
+	
+	public int getpoints(){
+		return points;
+	}
+	
+	public void setpoints(int point){
+		points=point;
+	}
+	
+	public String getfile(){
+		return mainfile;
+	}
+	
+	public void setfile(String f){
+		mainfile=f;
+	}
+	
+	public static void readgraph(String files){
+		try {
+	           
+            //System.out.print("Enter the file name with extention : ");
+            File file = new File(files);
+
+            Scanner input = new Scanner(file);
+            
+             points = (int)input.nextDouble();
+             mat=new double[(int) points][(int) points];
+            
+            System.out.println("points "+ points);
+            double edges=input.nextDouble();
+            System.out.println("edges "+edges);
+           while (input.hasNextDouble()) {
+        	  double a = input.nextDouble();
+        	  double b=input.nextDouble();
+        	  double c=input.nextDouble();
+        	  if(c<=0){
+        		  System.out.println("no negative input allowed");
+        	  	  return;}
+        	  
+        	  mat[(int)a][(int)b]=c;
+                
+               // System.out.println(line);
+               
+            }
+            
+            input.close();
+
+           /* for(int i=0;i<points;i++){
+            	for(int j=0;j<points;j++){
+            		System.out.print(mat[i][j]+" ");
+            	}
+            	System.out.println("");
+            }*/
+            
+            
+            
+            
+            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    
+
+	}
+	
+	
+		public static void isdirected(double a[][]){
+			for(int i=0;i<mat.length;i++){
+            	for(int j=0;j<mat[0].length;j++){
+            		if(mat[i][j]!=mat[j][i]){
+            			System.out.println("directed graph");
+            			return;
+            		}
+            	}
+            }
+			System.out.println("not a directed graph");
+		}
+		
+		
+		public static void blacklist(String files){
+			try {
+		           
+	            //System.out.print("Enter the file name with extention : ");
+	            File file = new File(files);
+
+	            Scanner input = new Scanner(file);
+	            
+	            double checks = input.nextDouble();
+	             
+	            
+	            System.out.println("checks "+ checks);
+	            
+	           
+	           while (input.hasNextDouble()) {
+	        	  
+	        	  
+	        	while(checks>0){
+	        		readgraph(mainfile);
+	        	  double a = input.nextDouble();
+	        	  double b=input.nextDouble();
+	        	  double c=input.nextDouble();
+	        	  if(c>0){
+	        		  double temp[]=new double[(int)c];
+	        		  System.out.println(c);
+	        		  for(int i=0;i<temp.length;i++){
+	        			  temp[i]=input.nextDouble();
+	        			  System.out.println(temp[i]);
+	        		  }
+	        		  for(int i=0;i<temp.length;i++){
+	        			  for(int j=0;j<mat.length;j++){
+	        				  mat[i][j]=Double.POSITIVE_INFINITY;
+	        				  mat[j][i]=Double.POSITIVE_INFINITY;
+	        			  }
+	        		  }
+	        	  }
+	        	   //mat[(int)a][(int)b]=c;
+	        	   checks--;
+	        	}
+	               // System.out.println(line);
+	               
+	            }
+	            
+	            input.close();
+
+	           /* for(int i=0;i<points;i++){
+	            	for(int j=0;j<points;j++){
+	            		System.out.print(mat[i][j]+" ");
+	            	}
+	            	System.out.println("");
+	            }*/
+	            
+	            
+	            
+	            
+	            
+	        } catch (Exception ex) {
+	            ex.printStackTrace();
+	        }
+		}
+		
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	 readgraph("C:\\res\\tinyEWD.txt");  
+	 isdirected(mat);
+	 blacklist("C:\\res\\test1.txt");
+	 for(int i=0;i<mat.length;i++){
+     	for(int j=0;j<mat[0].length;j++){
+     		System.out.print(mat[i][j]+" ");
+     	}
+     	System.out.println("");
+     }
+	 readgraph("C:\\res\\tinyEWG.txt"); 
+	 isdirected(mat);
+	 //readgraph("C:\\res\\mediumEWD.txt");
+	 //isdirected(mat);
+	 //readgraph("C:\\res\\mediumEWG.txt");
+	 //isdirected(mat);
+	}
+}
+
---
+++ readingfiles/src/readingfiles/Graph_algo.java
@@ -0,0 +1,134 @@
+package readingfiles;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+public class Graph_algo {
+
+	static // A Java program for Dijkstra's single source shortest path algorithm.
+	// The program is for adjacency matrix representation of the graph
+	
+	 
+	class ShortestPath
+	{
+	    // A utility function to find the vertex with minimum distance value,
+	    // from the set of vertices not yet included in shortest path tree
+	    //static final int V=9;
+	    double minDistance(double dist[], Boolean sptSet[],int V)
+	    {
+	        // Initialize min value
+	        double min = Integer.MAX_VALUE, min_index=-1;
+	 
+	        for (int v = 0; v < V; v++)
+	            if (sptSet[v] == false && dist[v] <= min)
+	            {
+	                min = dist[v];
+	                min_index = v;
+	            }
+	 
+	        return min_index;
+	    }
+	 
+	    // A utility function to print the constructed distance array
+	    void printSolution(double dist[], int n,int x,int y)
+	    {
+	        System.out.println("Vertex   Distance from Source");
+	        //for (int i = 0; i < V; i++)
+	           // System.out.println(i+" \t\t "+dist[i]);
+	        System.out.println(x+" to "+y+" is ");
+	        System.out.println(dist[y]);
+	    
+	    }
+	 
+	    // Funtion that implements Dijkstra's single source shortest path
+	    // algorithm for a graph represented using adjacency matrix
+	    // representation
+	    void dijkstra(double graph[][], int src,int dest,int V)
+	    {
+	        double dist[] = new double[V]; // The output array. dist[i] will hold
+	                                 // the shortest distance from src to i
+	 
+	        // sptSet[i] will true if vertex i is included in shortest
+	        // path tree or shortest distance from src to i is finalized
+	        Boolean sptSet[] = new Boolean[V];
+	 
+	        // Initialize all distances as INFINITE and stpSet[] as false
+	        for (int i = 0; i < V; i++)
+	        {
+	            dist[i] = Integer.MAX_VALUE;
+	            sptSet[i] = false;
+	        }
+	 
+	        // Distance of source vertex from itself is always 0
+	        dist[src] = 0;
+	 
+	        // Find shortest path for all vertices
+	        for (int count = 0; count < V-1; count++)
+	        {
+	            // Pick the minimum distance vertex from the set of vertices
+	            // not yet processed. u is always equal to src in first
+	            // iteration.
+	            double u = minDistance(dist, sptSet,V);
+	 
+	            // Mark the picked vertex as processed
+	            sptSet[(int)u] = true;
+	            //System.out.println("from "+u);
+	            // Update dist value of the adjacent vertices of the
+	            // picked vertex.
+	            for (int v = 0; v < V; v++){
+	 
+	                // Update dist[v] only if is not in sptSet, there is an
+	                // edge from u to v, and total weight of path from src to
+	                // v through u is smaller than current value of dist[v]
+	                if (!sptSet[v] && graph[(int)u][v]!=0 &&
+	                        dist[(int)u] != Integer.MAX_VALUE &&
+	                        dist[(int)u]+graph[(int)u][v] < dist[v]){
+	                	//System.out.println(v);
+	                    dist[v] = dist[(int)u] + graph[(int)u][v];}
+	            }
+	           // System.out.println(u);
+	        }
+	 
+	        
+	        
+	        // print the constructed distance array
+	        printSolution(dist, V,src,dest);
+	    }
+	 
+	    // Driver method
+	    public static void main (String[] args)
+	    {
+	    	Graph a=new Graph();
+	    	a.readgraph("C:\\res\\tinyEWD.txt");
+	    	double mat[][]=a.graph();
+	        /* Let us create the example graph discussed above */
+	       double graph[][] = new double[][]{{0, 4, 0, 0, 0, 0, 0, 8, 0},
+	                                  {4, 0, 8, 0, 0, 0, 0, 11, 0},
+	                                  {10, 8, 0, 7, 0, 4, 0, 0, 2},
+	                                  {0, 0, 7, 0, 9, 14, 0, 0, 0},
+	                                  {0, 0, 0, 9, 0, 10, 0, 0, 0},
+	                                  {0, 0, 4, 0, 10, 0, 2, 0, 0},
+	                                  {0, 0, 0, 14, 0, 2, 0, 1, 6},
+	                                  {8, 11, 0, 0, 0, 0, 1, 0, 7},
+	                                  {0, 0, 2, 0, 0, 0, 6, 7, 0}
+	                                 };
+	        ShortestPath t = new ShortestPath();
+	        
+	        t.dijkstra(graph, 7,2,9);
+
+           /* for(int i=0;i<mat.length;i++){
+            	for(int j=0;j<mat[0].length;j++){
+            		System.out.print(mat[i][j]+" ");
+            	}
+            	System.out.println("");
+            }*/
+	        ShortestPath g = new ShortestPath();
+	       g.dijkstra(mat, 1, 5, a.getpoints());
+	       g.dijkstra(mat, 5, 1, a.getpoints());
+	    }
+	    
+	}
+	
+}
